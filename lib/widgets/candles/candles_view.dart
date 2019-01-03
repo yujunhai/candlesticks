50fe714feda263af37aea112bc260a64e1de4188
johnleeker <johnleeker@hotmail.com>
2019-01-03 15:25:17 +0800

    update

--- lib/widgets/candles/candles_view.dart
+++ lib/widgets/candles/candles_view.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:candlesticks/widgets/candles/candles_state.dart';
 import 'package:candlesticks/2d/uiobjects/uio_candles.dart';
 import 'package:candlesticks/2d/uiwidget.dart';
-import 'package:candlesticks/widgets/UIGestureDetector.dart';
+import 'package:candlesticks/widgets/uigesture_detector.dart';
 
 class CandlesView extends CandlesState {
 
