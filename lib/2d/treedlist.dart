7763ca6c83c58f31820b6af24a0fd3adef412a1b
johnleeker <johnleeker@hotmail.com>
2019-01-04 04:16:55 +0800

    update

--- lib/2d/treedlist.dart
+++ lib/2d/treedlist.dart
@@ -2,12 +2,12 @@ library treedlist;
 
 class TreedListMin<T extends Comparable> {
     List<T> _data;
-    List<T> _minData;
+    List<int> _minData;
     final int reverse;
 
     TreedListMin(List<T> data, {this.reverse = 1}) {
         this._data = <T>[];
-        this._minData = <T>[null];
+        this._minData = <int>[null];
         if (data != null) {
             for (var i = 0; i < data.length; i++) {
                 this.add(data[i]);
@@ -45,9 +45,9 @@ class TreedListMin<T extends Comparable> {
         }
         _data[index] = value;
         for (int i = index + 1; i <= _data.length; i += i & -i) {
-            _minData[i] = _data[i - 1];
+            _minData[i] = i - 1;
             for (int j = 1; j < i & -i; j <<= 1) {
-                if ((_minData[i - j]).compareTo(_minData[i]) * reverse < 0) {
+                if ((_data[_minData[i - j]]).compareTo(_data[_minData[i]]) * reverse < 0) {
                     _minData[i] = _minData[i - j];
                 }
             }
@@ -55,6 +55,14 @@ class TreedListMin<T extends Comparable> {
     }
 
     T min(int left, int right) {
+        var index = minIndex(left, right);
+        if(index == null) {
+            return null;
+        }
+        return _data[index];
+    }
+
+    int minIndex(int left, int right) {
         if (right < left) {
             var t = left;
             left = right;
@@ -63,8 +71,8 @@ class TreedListMin<T extends Comparable> {
         if (left < 0) {
             left = 0;
         }
-        if (right <= 0) {
-            return null;
+        if (right < 0) {
+            right = 0;
         }
         if ((_data == null) || (_data.length <= 0)) {
             return null;
@@ -74,15 +82,15 @@ class TreedListMin<T extends Comparable> {
         }
         right++;
         left++;
-        T min = _data[right - 1];
+        int min = right - 1;
         while (true) {
-            if ((_data[right - 1]).compareTo(min) * reverse < 0) {
-                min = _data[right - 1];
+            if ((_data[right - 1]).compareTo(_data[min]) * reverse < 0) {
+                min = right - 1;
             }
             if (right == left) break;
             for (right -= 1; right - left >= right & -right;
             right -= right & -right) {
-                if ((_minData[right]).compareTo(min) * reverse < 0) {
+                if ((_data[_minData[right]]).compareTo(_data[min]) * reverse < 0) {
                     min = _minData[right];
                 }
             }
