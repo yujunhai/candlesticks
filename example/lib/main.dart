5f4035f99f1ec2daa598adf192fa7170d8340491
johnleeker <johnleeker@hotmail.com>
2019-01-06 16:20:52 +0800

    切换颜色还是有问题。。。。

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -17,19 +17,34 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int count = 0;
+
   @override
   void initState() {
     super.initState();
     dataStreamFuture = DataSource.instance.initTZB(5);
+    style = CandlesticksStyle(
+      backgroundColor: Color(0xff21232e),
+      cameraDuration: Duration(milliseconds: 500),
+      viewPortX: 40,
+      candlesStyle: CandlesStyle(
+          positiveColor: Colors.redAccent,
+          negativeColor: Colors.greenAccent,
+          paddingX: 0.5,
+          duration: Duration(milliseconds: 200)),
+      maStyle: MaStyle(
+          shortCount: 5,
+          maShort: Colors.yellowAccent,
+          middleCount: 15,
+          maMiddle: Colors.greenAccent,
+          longCount: 30,
+          maLong: Colors.deepPurpleAccent,
+          duration: Duration(milliseconds: 200)),
+    );
   }
 
   GlobalKey<KChartsState> kChartsKey = new GlobalKey<KChartsState>();
-
-  Future<Stream<CandleData>> getStream() async {
-    return DataSource.instance.subject.stream;
-  }
-
   Future<Stream<CandleData>> dataStreamFuture;
+  CandlesticksStyle style;
 
   @override
   Widget build(BuildContext context) {
@@ -45,15 +60,55 @@ class _MyAppState extends State<MyApp> {
                 RaisedButton(
                   onPressed: () {
                     count++;
-                    if(count % 2 == 0) {
+                    if (count % 4 == 0) {
                       dataStreamFuture = DataSource.instance.initTZB(1);
                       print('切换K线 1');
-                    } if(count % 2 == 1){
+                    }
+                    if (count % 4 == 1) {
                       dataStreamFuture = DataSource.instance.initTZB(5);
                       print('切换K线 5');
-                    }else {
-                      print('切换K线 5');
-                      dataStreamFuture = DataSource.instance.initTZB(5);
+                    } else if (count % 4 == 2) {
+//                      dataStreamFuture = DataSource.instance.initTZB(5);
+                      print('切换颜色 红绿');
+                      style = CandlesticksStyle(
+                        backgroundColor: Color(0xff21232e),
+                        cameraDuration: Duration(milliseconds: 500),
+                        viewPortX: 40,
+                        candlesStyle: CandlesStyle(
+                            positiveColor: Colors.redAccent,
+                            negativeColor: Colors.greenAccent,
+                            paddingX: 0.5,
+                            duration: Duration(milliseconds: 200)),
+                        maStyle: MaStyle(
+                            shortCount: 5,
+                            maShort: Colors.yellowAccent,
+                            middleCount: 15,
+                            maMiddle: Colors.greenAccent,
+                            longCount: 30,
+                            maLong: Colors.deepPurpleAccent,
+                            duration: Duration(milliseconds: 200)),
+                      );
+                    } else if (count % 4 == 3) {
+//                      dataStreamFuture = DataSource.instance.initTZB(5);
+                      print('切换颜色 绿红');
+                      style = CandlesticksStyle(
+                        backgroundColor: Color(0xff21232e),
+                        cameraDuration: Duration(milliseconds: 500),
+                        viewPortX: 40,
+                        candlesStyle: CandlesStyle(
+                            positiveColor: Colors.greenAccent,
+                            negativeColor: Colors.redAccent,
+                            paddingX: 0.5,
+                            duration: Duration(milliseconds: 200)),
+                        maStyle: MaStyle(
+                            shortCount: 5,
+                            maShort: Colors.yellowAccent,
+                            middleCount: 15,
+                            maMiddle: Colors.greenAccent,
+                            longCount: 30,
+                            maLong: Colors.deepPurpleAccent,
+                            duration: Duration(milliseconds: 200)),
+                      );
                     }
                     setState(() {
 
@@ -67,29 +122,13 @@ class _MyAppState extends State<MyApp> {
                 future: dataStreamFuture,
                 builder: (BuildContext context,
                     AsyncSnapshot<Stream<CandleData>> snapshot) {
-                  if((snapshot.connectionState != ConnectionState.done ) || (!snapshot.hasData) || (snapshot.data == null)) {
+                  if ((snapshot.connectionState != ConnectionState.done) ||
+                      (!snapshot.hasData) || (snapshot.data == null)) {
                     return Container();
                   }
                   return CandlesticksWidget(
                     dataStream: snapshot.data,
-                    candlesticksStyle: CandlesticksStyle(
-                      backgroundColor: Color(0xff21232e),
-                      cameraDuration: Duration(milliseconds: 500),
-                      viewPortX: 40,
-                      candlesStyle: CandlesStyle(
-                          positiveColor: Colors.redAccent,
-                          negativeColor: Colors.greenAccent,
-                          paddingX: 0.5,
-                          duration: Duration(milliseconds: 200)),
-                      maStyle: MaStyle(
-                          shortCount: 5,
-                          maShort: Colors.yellowAccent,
-                          middleCount: 15,
-                          maMiddle: Colors.greenAccent,
-                          longCount: 30,
-                          maLong: Colors.deepPurpleAccent,
-                          duration: Duration(milliseconds: 200)),
-                    ),
+                    candlesticksStyle:style,
                   );
                 }),
           ),
