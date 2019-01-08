56969a68a3320afe55d8b167453b09ed741a35ef
johnleeker <johnleeker@hotmail.com>
2019-01-08 21:02:55 +0800

    update

--- lib/widgets/candles/candles_value_widget.dart
+++ lib/widgets/candles/candles_value_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'dart:ui' as ui;
 import 'package:candlesticks/widgets/aabb/aabb_range.dart';
 import 'package:candlesticks/widgets/aabb/aabb_context.dart';
 import 'package:candlesticks/2d/uicamera.dart';
@@ -22,13 +23,18 @@ class CandlesValuePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    double dx = 20;
     Offset p = uiCamera.viewPortToScreenPoint(size, uiCamera.worldToViewPortPoint(point));
+    Offset pText = p + Offset(dx, 0);
+    String price = point.y.toStringAsFixed(4);
+
+
     TextPainter currentTextPainter = TextPainter(
         textDirection: TextDirection.ltr,
         maxLines: 1,
         textAlign: TextAlign.end,
         text: TextSpan(
-          text: "asdf",
+          text: "${price}",
           style: TextStyle(
             color: Colors.white,
             fontSize: 10.0,
@@ -36,7 +42,47 @@ class CandlesValuePainter extends CustomPainter {
         )
     );
     currentTextPainter.layout();
-    currentTextPainter.paint(canvas, p);
+//    p += Offset(0, -currentTextPainter.height / 2);
+    var pLeftTop = pText + Offset(0, -currentTextPainter.height / 2);
+    currentTextPainter.paint(canvas, pLeftTop);
+
+    var linePainter = Paint()..color=Colors.white;
+    canvas.drawLine(p, pText, linePainter);
+
+    Paint maxCircle = new Paint();
+//      maxCircle..color = kStyle.priceMinMaxFontColor.withOpacity(0.3);
+    maxCircle..shader = ui.Gradient.radial(pText, 3, [
+      Colors.white.withOpacity(0.8),
+      Colors.white.withOpacity(0.1),
+    ], [0.0, 1.0], TileMode.clamp);
+    canvas.drawCircle(p, 3, maxCircle);
+    /*
+    Map maxData = kPoint.kPointData[kPoint.kMaxPriceIndex];
+    TextPainter textMax = getTextPainter(maxData['high'].toString(), 1);
+    textMax.layout();
+
+    var maxX = maxData['klineX'];
+    var maxBorderX1 = maxData['klineX'];
+    var maxBorderX2 = maxData['klineX'] + kPoint.kPriceBorderW;
+    if(maxX > size.width / 2){
+      maxBorderX2 = maxBorderX1 - kPoint.kPriceBorderW;
+      maxX = maxBorderX2 - 2 - textMax.width;
+    }else{
+      maxBorderX2 = maxBorderX1 + kPoint.kPriceBorderW;
+      maxX = maxBorderX2 + 2;
+    }
+    var maxY = maxData['highY'] - (textMax.height / 2);
+    textMax.paint(canvas, new Offset(maxX, maxY));
+    canvas.drawLine(new Offset(maxBorderX1, maxData['highY']), new Offset(maxBorderX2, maxData['highY']), new Paint()..color = kStyle.priceMinMaxFontColor.withOpacity(0.5));
+    Paint maxCircle = new Paint();
+//      maxCircle..color = kStyle.priceMinMaxFontColor.withOpacity(0.3);
+    maxCircle..shader = new ui.Gradient.radial(new Offset(maxBorderX1, maxData['highY']), kPoint.kMinMaxPriceCircle, [
+      kStyle.priceMinMaxFontColor.withOpacity(0.8),
+      kStyle.priceMinMaxFontColor.withOpacity(0.1),
+    ], [0.0, 1.0], TileMode.clamp);
+    canvas.drawCircle(new Offset(maxBorderX1, maxData['highY']), kPoint.kMinMaxPriceCircle, maxCircle);
+    */
+
   }
 
   @override
