c076a2e65dd213c9af72e492ae9d3f946d7bdefc
johnleeker <johnleeker@hotmail.com>
2019-01-11 14:46:56 +0800

    update

--- lib/widgets/candlesticks_style.dart
+++ lib/widgets/candlesticks_style.dart
@@ -3,7 +3,44 @@ import 'package:candlesticks/widgets/ma/ma_style.dart';
 import 'package:candlesticks/widgets/candles/candles_style.dart';
 import 'package:candlesticks/widgets/floating/floating_style.dart';
 
+
 CandlesticksStyle DefaultDarkCandleStyle = CandlesticksStyle(
+  backgroundColor: Color(0xFF060827),
+  cameraDuration: Duration(milliseconds: 500),
+  initAfterNData: 50,
+  defaultViewPortX: 50,
+  minViewPortX: 20,
+  maxViewPortX: 128,
+  fractionDigits:4,
+  lineColor: Colors.white.withOpacity(0.2),
+  nX: 5,
+  nY: 4,
+  floatingStyle: FloatingStyle(
+    backGroundColor: Colors.black.withOpacity(0.8),
+    frontSize: 10,
+    borderColor: Colors.white,
+    frontColor: Colors.white,
+    crossColor: Colors.white,
+  ),
+  candlesStyle: CandlesStyle(
+      positiveColor: Colors.redAccent,
+      negativeColor: Colors.greenAccent,
+      paddingX: 0.5,
+      cameraPaddingY: 0.1,
+      duration: Duration(milliseconds: 200)),
+  maStyle: MaStyle(
+      currentColor:Colors.white.withOpacity(0.85),
+      cameraPaddingY: 0.2,
+      shortCount: 5,
+      shortColor: Colors.yellowAccent,
+      middleCount: 15,
+      middleColor: Colors.greenAccent,
+      longCount: 30,
+      longColor: Colors.deepPurpleAccent,
+      duration: Duration(milliseconds: 200)),
+);
+
+CandlesticksStyle DefaultLightCandleStyle = CandlesticksStyle(
   backgroundColor: Color(0xff21232e),
   cameraDuration: Duration(milliseconds: 500),
   initAfterNData: 50,
